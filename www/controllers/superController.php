adc1fc7e9a5178b88ff9f4e14d2b4d17a44b1d0c
Guillaume <le.clerc.guillaume@icloud.com>
2015-05-19 23:38:03 +0200

    Ajout des methodes render, isConnected et isAdmin dans superController.php

---
+++ www/controllers/superController.php
@@ -0,0 +1,37 @@
+<?php
+
+class superController{
+
+    private $userConnect = false;
+    private $userIsAdmin = false;
+
+    protected function render($tab){
+        extract($tab); // Extraction du tableau
+        ob_start(); // Demarage de l'outpout buffering
+        include(''); // IL FAUT INCLURE LA VUE CONCERNER DYNAMIQUEMENT
+        $content = ob_get_contents(); // stockage du contenu de l'outpout buffering dans une variable
+        ob_clean(); //vidage de la memoire
+        include('..' . DIRECTORY_SEPARATOR . 'views' . DIRECTORY_SEPARATOR . 'layout.php');
+    }
+
+    public function isConnected(){
+        if(!empty($_SESSION['user'])){
+            $this->userConnect = true;
+            return $this->userConnect;
+        } else {
+            return 'Veuillez vous connecté ! '; // AJOUTER LE LIEN VERS FORMULAIRE DE CONNEXIO
+        }
+    }
+
+    public function isAdmin(){
+        if($this->userConnect == true && $_SESSION['user']['statut'] == 1){
+            $this->userIsAdmin = true;
+            return $this->userIsAdmin;
+        } else {
+            $this->userIsAdmin = false;
+            return 'Vous n\êtes pas administrateur !';
+        }
+    }
+}
+
+?>
