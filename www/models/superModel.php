5f44377f1bce385d013b452220240864c254e365
PgAuffret <pg.auffret@outlook.fr>
2015-05-20 09:13:48 +0200

    Update superModel.php
    <?php
    class Super_model
    {
    	function super_model()
    	{
    		require('model_config.php');
    		//infos_connexion();
    		//$pdo = new PDO("mysql:host=".$infosConnexion['host'].";dbname=".$infosConnexion['dbname'],$infosConnexion['user'], $infosConnexion['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
    		$pdo = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
    		$pdo->exec("SET CHARACTER SET utf8");
    		return $pdo;
    	}
    }
    //$pdo = new PDO('mysql:host=localhost;dbname=symfony', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));

--- www/models/superModel.php
+++ www/models/superModel.php
@@ -18,3 +18,18 @@ class superModel{
 }
 
 ?>
+// Pg
+<?php
+class Super_model
+{
+	function super_model()
+	{
+		require('model_config.php');
+		//infos_connexion();
+		//$pdo = new PDO("mysql:host=".$infosConnexion['host'].";dbname=".$infosConnexion['dbname'],$infosConnexion['user'], $infosConnexion['password'], array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$pdo = new PDO("mysql:host=" . $host . ";dbname=" . $dbname, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+		$pdo->exec("SET CHARACTER SET utf8");
+		return $pdo;
+	}
+}
+//$pdo = new PDO('mysql:host=localhost;dbname=symfony', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
