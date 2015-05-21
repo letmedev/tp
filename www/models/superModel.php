750baa453cb4fa4a6b3f966bdc6fb76f78a3b6b6
Guillaume <le.clerc.guillaume@icloud.com>
2015-05-21 11:52:07 +0200

    Correction des erreurs du superModel (problème de cotte)

--- www/models/superModel.php
+++ www/models/superModel.php
@@ -1,20 +1,17 @@
 <?php
 class superModel{
 
-    private $bdd;
-
-    protected function getDatabase(){
-
-        //require_once('..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'host.php');
-        require_once('../config/host.php');
+    public function getDatabase(){
+        require_once('..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'host.php');
         try{
-            $this->bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8,' . $login . ',' . $password);
+            $bdd = new PDO('mysql:host='. $host .';dbname='. $dbname .';charset=utf8', $login, $password);
         } catch (Exeception $e){
             die('Une erreur est survenue: ' . $e->getMessage());
         }
-        return $this->bdd;
+        return $bdd;
     }
 
 }
+
 
 ?>
