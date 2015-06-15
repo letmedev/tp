7a100f0d93068960610445dcf0249aa31cb50547
Guillaume <le.clerc.guillaume@icloud.com>
2015-06-15 10:04:41 +0200

    Ajout du formulaire de contact, Obliger d'utiliser phpmailer car l'hebergeur exige une authentification pour l'envoi de mail. Formulaire fonctionnel

--- www/controllers/aproposController.php
+++ www/controllers/aproposController.php
@@ -24,6 +24,57 @@ namespace controller\aproposController{
 
         public function contact(){
 
+            if(isset($_POST) && !empty($_POST)){
+                if(isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['message'])){
+                    if(!empty($_POST['nom']) && !empty($_POST['prenom']) && !empty($_POST['email']) &&!empty($_POST['message'])){
+                        extract($_POST);
+
+                        //===================================================================================
+                        //                      MESSAGE A L'ATTENTION DU CORRECTEUR.
+                        // Je suis obligé d'utiliser PHPMail car mon hebergeur (Ikoula) n'autorise pas
+                        // l'envoi d'email s'il n'y a pas d'autentification et la fonction mail() de php
+                        // ne le permet pas.
+                        //===================================================================================
+
+
+                        include('../public/script/PHPMailer/class.phpmailer.php');
+                        include('../public/script/PHPMailer/class.smtp.php');
+
+                        $mail = new \PHPMailer();
+
+                        $body = "Vous venez de recevoir un message de la part de " . $nom . ' ' . $prenom  . "<br /><br />";
+                        $body .= "Voici son adresse email: <br />" . $email . "<br /><br />";
+                        $body .= "Voici son message: <br />" . $message . "<br /><br />";
+
+                        $mail->isSMTP();
+                        $mail->SMTPAuth = true;
+                        $mail->Host = "mail.gldev.fr";
+                        $mail->Port = 25;
+
+                        $mail->Username = "contact@gldev.fr";
+                        $mail->Password = "ds7sdrk45";
+                        $mail->From = "contact@gldev.fr";
+                        $mail->FromName = "Contact Lokisalle";
+                        $mail->Subject = "Vous avez un contact provenant de lokisalle";
+                        $mail->AltBody = $body;
+
+                        $mail->WordWrap = 70;
+                        $mail->MsgHTML($body);
+
+                        $mail->AddReplyTo($email);
+                        $mail->AddAddress("contact@gldev.fr");
+                        $mail->isHTML(true);
+
+                        if($mail->Send()){
+                            $this->msg .= "<div class='msgSuccess'>Votre message à bien été envoyer! <br /> Vous aurez un retour d'ici 48H.</div>";
+                        } else{
+                            $this->msg .= "<div class='msgAlert'>Une erreure c'est produite mercie de réessayer dans un moment. Si le problème persiste contacter le support.</div>";
+                        }
+                    }
+                } else{
+                    $this->msg .= "<div class='msgAlert'>Tout les informations demandé doivent être remplit afin de pouvoir valider le formulaire.</div>";
+                }
+            }
 
             $tab = array(
                 'msg' => $this->getMsg(),
--- www/views/apropos/contactView.php
+++ www/views/apropos/contactView.php
@@ -1,12 +1,12 @@
 <h1 class="titrePageSalle marginTop40">Formulaire de contact</h1>
 <form action="<?php echo \controller\superController\superController::URL ?>apropos/contact" method="post" class="formContact marginTop40">
     <label for="nom">Nom:</label>
-    <input type="text" id="nom" name="nom" placeholder="Nom…"/>
+    <input type="text" id="nom" name="nom" placeholder="Nom…" required/>
     <label for="prenom">Prénom:</label>
-    <input type="text" id="prenom" name="prenom" placeholder="Prénom…"/>
+    <input type="text" id="prenom" name="prenom" placeholder="Prénom…" required/>
     <label for="email">Email:</label>
-    <input type="email" id="email" name="email" placeholder="exemple@mail.com"/>
+    <input type="email" id="email" name="email" placeholder="exemple@mail.com" required/>
     <label for="message">Votre message:</label>
-    <textarea name="message" id="message" class="textareaContact"></textarea>
+    <textarea name="message" id="message" class="textareaContact" required></textarea>
     <input type="submit" class="btnFormContact" value="Envoyer" name="btnFormContact"/>
 </form>
